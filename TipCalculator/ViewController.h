9dc1dafd98764ccb7df892db2d3460ee36c2ab2d
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-20 08:52:11 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Alejandro Zielinsky on 2018-04-20.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
